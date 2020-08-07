75573eb25ae1f27b5d075914354f6478fcd6b68f
susanabte <68082394+susanabte@users.noreply.github.com>
2020-08-07 15:38:00 -0500

    Add files via upload

--- dados 2.0.cs
+++ dados 2.0.cs
@@ -10,6 +10,7 @@ public class Program
     int d1 = 0, total = 0, contador = 0, ronda = 0;
     string continuar = "s";
 
+    
     while (continuar == "s" && total < 100) 
     {
         d1 = aleatorio.Next(1,13);
@@ -18,19 +19,7 @@ public class Program
 		Console.WriteLine("total:" + total);
 		ronda +=1;
         
-        if (ronda > 3)
-        {
-        if (d1%2 != 0)
-        {
-            total = 0;
-            Console.WriteLine("perdiste");
-            continuar = "N";
-        }
-        if (d1 == 12 )
-        {
-            contador += 1; 
-        }
-
+    
         if (contador == 1)
         {
             if (d1 == 10)
@@ -41,6 +30,21 @@ public class Program
 			{
 				contador = 0;
 			}
+        }
+        if (d1 == 12 )
+        {
+            contador += 1; 
+        }
+ 
+        
+        if (ronda > 3)
+        {
+        if (d1%2 != 0)
+        {
+            total = 0;
+            Console.WriteLine("perdiste");
+            continuar = "N";
+        }
         }
 
        if(total >= 100 || contador == 2)
@@ -57,8 +61,6 @@ public class Program
         continuar = Console.ReadLine();
         }
     }
-
-    
 }
 }
 
